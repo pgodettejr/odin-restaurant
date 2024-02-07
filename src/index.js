49a0ba279597d7de5a6049ab63a55db98309778c
pgodettejr <paul.godette@gmail.com>
2024-02-07 12:01:23 -0500

    Load CSS and images to project via webpack
    Loaded CSS and images to the project despite not being part of TOP's
    instructions as CSS along with images files including the logo and
    other pictures would be needed for the planned homepage layout.
    Everything was done via the Asset Management tutorial in Webpack loosely
    step-by-step.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
+import './styles.css';
+// import Logo from './img/wreath-6524584_640.png';
+
+// const myLogo = new Image();
+// myLogo.src = Logo;
+
+// ???.appendChild(myLogo);
+
 console.log('FOOD!');
