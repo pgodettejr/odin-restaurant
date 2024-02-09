0b1e757fab2b993f30595a3882525c69a2750c08
pgodettejr <paul.godette@gmail.com>
2024-02-09 16:39:25 -0500

    Modify JS code to debug 404 error image issue
    Removed comments from previous JS code, then modified that code in an
    attempt to fix the 404 error showing for all images on the homepage
    (they are showing as "broken"). Error still persists.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import './styles.css';
-// import Logo from './img/wreath-6524584_640.png';
+import Logo from './img/wreath-6524584_640.png';
 
-// const myLogo = new Image();
-// myLogo.src = Logo;
-
-// ???.appendChild(myLogo);
+// Potentially place all imported elements inside it's own "homepage" function and append that function to the HTML body using the comment at the bottom
+const myLogo = document.querySelector('logo');
+myLogo.img.src = Logo;
 
 console.log('FOOD!');
+
+// document.body.appendChild(homepage());
