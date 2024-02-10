1b21487d5ed8a498354933996d36461ea600d37e
pgodettejr <paul.godette@gmail.com>
2024-02-10 12:57:17 -0500

    Refactor and edit JS code for image debugging
    Refactored and modified code inside of JS in an attempt to debug the
    404 error "broken images" that are on the homepage. Error still
    persists. Added comments to webpack config file regarding possibly
    turning watch mode "on" (though it should already BE on) and a note to
    find out why the images assets aren't listed as a dependency in the
    package.json file and whether or not they should be (would this solve
    the 404 error?).

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import './styles.css';
-import Logo from './img/wreath-6524584_640.png';
+import Wreath from './img/wreath-6524584_640.png';
 
 // Potentially place all imported elements inside it's own "homepage" function and append that function to the HTML body using the comment at the bottom
-const myLogo = document.querySelector('logo');
-myLogo.img.src = Logo;
+const logo = document.querySelector('logo');
+
+const myLogo = new Image();
+myLogo.src = Wreath;
+
+logo.appendChild(myLogo);
 
 console.log('FOOD!');
 
--- webpack.config.js
+++ webpack.config.js
@@ -6,12 +6,14 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  // watch: true,
   module: {
     rules: [
       {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
       },
+      // Why isn't the image rule below a dev dependency for Webpack in the package.json file?
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
