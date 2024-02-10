3e0c66024ddbb1e27d868660ace7c9f17b6cfc72
pgodettejr <paul.godette@gmail.com>
2024-02-10 14:05:14 -0500

    Add comment for return statement on JS

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import './styles.css';
 import Wreath from './img/wreath-6524584_640.png';
 
-// Potentially place all imported elements inside it's own "homepage" function and append that function to the HTML body using the comment at the bottom
+// Potentially place all imported elements inside it's own "homepage" function and append that function to the HTML body using the comment at the bottom (need: return "variable")
 const logo = document.querySelector('logo');
 
 const myLogo = new Image();
