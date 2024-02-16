96ca190376cdf82b2b57fe9d7ea00f23688f76c5
pgodettejr <paul.godette@gmail.com>
2024-02-16 11:18:31 -0500

    Modify image attributes and add gallery subtext
    Finished adding the empty subtext divs to each image section of the
    gallery info section of the homepage. Refactored all images to have an
    'alt' attribute instead of a class describing the image. Appended all
    major homepage sections to the content div.

--- src/home.js
+++ src/home.js
@@ -20,12 +20,13 @@ export function homepage() {
   // Image for the 'media' section of the homepage
   let mediaImage = document.createElement('img');
   mediaImage.src = '.img/greek-523731_1920.jpg';
+  mediaImage.setAttribute = ('alt', 'restaurant-pic-1');
   media.appendChild(mediaImage);
 
   // Background image, header and text for the 'Our Philosophy' section of the homepage
   let foodImage = document.createElement('img');
   foodImage.src = '.img/table-2931360_1920.jpg';
-  foodImage.classList.add('food-bg');
+  foodImage.setAttribute('alt', 'restaurant-pic-2');
   food.appendChild(foodImage);
 
   const foodHeader = document.createElement('h2');
@@ -77,26 +78,42 @@ export function homepage() {
 
   let galleryPic1 = document.createElement('img');
   galleryPic1.src = '.img/meat-6507228_640.jpg';
-  galleryPic1.classList.add('meat-plate');
+  galleryPic1.setAttribute('alt', 'meat-plate');
   gallery1.appendChild(galleryPic1);
 
   let galleryPic2 = document.createElement('img');
   galleryPic2.src = '.img/gyros-674425_640.jpg';
-  galleryPic2.classList.add('gyro');
+  galleryPic2.setAttribute('alt', 'gyro');
   gallery2.appendChild(galleryPic2);
 
   let galleryPic3 = document.createElement('img');
   galleryPic3.src = '.img/food-3337621_640.jpg';
-  galleryPic3.classList.add('salad');
+  galleryPic3.setAttribute('alt', 'salad');
   gallery3.appendChild(galleryPic3);
 
   let galleryPic4 = document.createElement('img');
   galleryPic4.src = '.img/table-2931360_1920.jpg';
-  galleryPic4.classList.add('table');
+  galleryPic4.setAttribute('alt', 'table');
   gallery4.appendChild(galleryPic4);
 
   // Subtext divs for the 'Gallery' section. Trying to implement a forEach method for these as they will be empty divs with a class of 'subtext'
-  // document.querySelectorAll('div[class^="gallery-"]').forEach((element) => {
-  //   element.classList.add('subtext');
-  // })
+  document.querySelectorAll('div[class^="gallery-"]').forEach((element) => {
+    let subtext = document.createElement('div');
+    subtext.classList.add('subtext');
+    element.appendChild(subtext);
+  })
+
+  // Attaching all gallery elements to the 'Gallery' section
+  galleryInfo.appendChild(gallery1);
+  galleryInfo.appendChild(gallery2);
+  galleryInfo.appendChild(gallery3);
+  galleryInfo.appendChild(gallery4);
+
+  // Attaching all main sections (Media, Our Philosophy, About Us, Testimonial, Gallery) to the existing content div
+  content.appendChild(media);
+  content.appendChild(food);
+  content.appendChild(about);
+  content.appendChild(testimonial);
+  content.appendChild(galleryHeader);
+  content.appendChild(galleryInfo);
 }
