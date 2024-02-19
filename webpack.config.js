ac01e7874962eaadb6bf283bdcadbc50ae10f0bb
pgodettejr <paul.godette@gmail.com>
2024-02-19 10:53:55 -0500

    Set up webpack-dev-server to automate workflow
    Set up webpack-dev-server and added the necessary scripts and devServer
    mode to automate the process of loading the webpage each time a change
    is made in any of the project files. Also switched webpack config to
    development mode.

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,23 @@
 const path = require('path');
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
+  // devtool: 'eval',
+  devServer: { // Delete this if we try the peterxjang method for webpack-dev-server (also delete "start" line in package.json file)
+    static: './dist',
+  },
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
   // watch: true,
+
+  // This is only needed if we have more than 1 entry point, which we don't currently. Delete this regardless if we try the peterxjang method for webpack-dev-server
+  // optimization: { 
+  //   runtimeChunk: 'single',
+  // },
+
   module: {
     rules: [
       {
