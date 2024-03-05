7ae853791e89a016779348f91e6a62e7e519ed88
pgodettejr <paul.godette@gmail.com>
2024-03-05 12:29:06 -0500

    Modify comments and remove console log test code
    Removed comments that were no longer needed or have been resolved. Added
    a comment regarding possible approach to button event listeners for the
    tabbed browsing. Removed console log statement that tested the initial
    page as it was no longer needed.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-// Uncaught TypeError: Failed to execute 'appendChild' on 'Node': parameter 1 is not of type 'Node' <-- does this have to do with line 19 or not having "webpack require" config?
+// Uncaught TypeError: Failed to execute 'appendChild' on 'Node': parameter 1 is not of type 'Node' <-- does this have to do with line 17 or not having "webpack require" config?
 
 import './styles.css';
 // import Wreath from './img/wreath-6524584_640.png';
 import { homepage } from './home.js';
 
-// Potentially place all imported elements inside it's own "homepage" function and append that function to the HTML body using the comment at the bottom (need: return "variable")
+// Will need the 'replaceChildren()' function in order to get the tabbed browsing to work in our event listener for the nav buttons (see: odin-library and tic-tac-toe JS for e.g.)
 // Add all button logic to this file (event listener to remove all current elements under content div and replace with { imported } elements related to which button was clicked)
 // const logo = document.querySelector('.logo');
 const content = document.getElementById('content');
@@ -13,7 +13,5 @@ const content = document.getElementById('content');
 // myLogo.src = Wreath;
 
 // logo.appendChild(myLogo);
-
-console.log('FOOD!');
 
 content.appendChild(homepage());
