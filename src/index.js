eb550606771b42748bd8d8371a478ca6713365d6
pgodettejr <paul.godette@gmail.com>
2024-03-12 12:02:49 -0400

    Modify code in an attempt to fix TypeError
    Tried to add a 'try catch' method to further drill down on the TypeError
    I was getting on the last line of code (parameter not being a Node when
    appendChild tries to execute). Also moved the console.log above which
    allowed the console to correctly identify the typeof, which is a
    function. Attempted to add a parameter to the function since it was
    empty, but didn't work. I've now realized it's the function ITSELF that
    isn't a Node when appendChild is called. Possibly changing the setup of
    how the content div is being called may resolve the issue.

--- src/index.js
+++ src/index.js
@@ -51,7 +51,7 @@ navButtons.forEach(button => {
 
 // logo.appendChild(myLogo);
 
-// TODO: Uncaught TypeError: Failed to execute 'appendChild' on 'Node': parameter 1 is not of type 'Node' is preventing the tabbed browsing <-- does this have to do with not having "webpack require" config?
-content.appendChild(homepage());
+// console.log(typeof homepage); <-- The type is a function
 
-console.log(typeof homepage);
+// TODO: Uncaught TypeError: Failed to execute 'appendChild' on 'Node': parameter 1 is not of type 'Node' is preventing the tabbed browsing <-- does this have to do with not having "webpack require" config? Or is a function not considered a Node for the appendChild method?
+content.appendChild(homepage());
