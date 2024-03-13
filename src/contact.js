5f9636e30a9531f9c8174c275b8b8c6547be1d8a
pgodettejr <paul.godette@gmail.com>
2024-03-13 12:45:22 -0400

    Add form section to the Contact page
    Added most of the elements of the form section, including sections for
    filling out Name, E-Mail, Subject and a Submit button. The only thing
    missing is the Message box which will need a bit of research.

--- src/contact.js
+++ src/contact.js
@@ -22,8 +22,65 @@ function contactPage() {
 
   contactSection.appendChild(contactForm);
 
-  // Form column divs within the form section that act as containers for each section of the form
+  // Name section of the form
+  const formColumn1 = document.createElement('div');
+  formColumn1.setAttribute('id', 'form-col-1');
+  contactForm.appendChild(formColumn1);
+
+  const formLabel1 = document.createElement('label');
+  formLabel1.setAttribute('for', 'contact-name');
+  formLabel1.textContent = 'Name';
+
+  const formInput1 = document.createElement('input');
+  multiAttributes(formInput1, {"type": "text", "name": "contact-name", "id": "contact-name"});
+  formInput1.required = true;
+
+  formColumn1.appendChild(formLabel1, formInput1);
+
+  // Email section of the form
+  // TODO: Check to see if there are any different requirements for an e-mail section of the form (like the 'type' attribute, etc)
+  const formColumn2 = document.createElement('div');
+  formColumn2.setAttribute('id', 'form-col-2');
+  contactForm.appendChild(formColumn2);
+
+  const formLabel2 = document.createElement('label');
+  formLabel2.setAttribute('for', 'contact-email');
+  formLabel2.textContent = 'E-Mail';
+
+  const formInput2 = document.createElement('input');
+  multiAttributes(formInput2, {"type": "text", "name": "contact-email", "id": "contact-email"});
+  formInput2.required = true;
+
+  formColumn2.appendChild(formLabel2, formInput2);
+
+  // Subject/Title section of the form
+  const formColumn3 = document.createElement('div');
+  formColumn3.setAttribute('id', 'form-col-3');
+  contactForm.appendChild(formColumn3);
+
+  const formLabel3 = document.createElement('label');
+  formLabel3.setAttribute('for', 'contact-subject');
+  formLabel3.textContent = 'Subject';
+
+  const formInput3 = document.createElement('input');
+  multiAttributes(formInput3, {"type": "text", "name": "contact-subject", "id": "contact-subject"});
+  formInput3.required = true;
+
+  formColumn3.appendChild(formLabel3, formInput3);
+
+  // Message section of the form goes below this line
+  // TODO: Research/read docs on how to set up a Message box properly
+
   
+
+  // Submit button for the form
+  const submitBtn = document.createElement('button');
+  const submitBtnText = document.createTextNode('Submit');
+  submitBtn.classList.add('submit-btn');
+
+  submitBtn.appendChild(submitBtnText);
+  contactForm.appendChild(submitBtn);
+
 
   // // My attempt to make multiple containers at once to house each form section in, but couldn't figure out how to add the sections themselves under each div created
   // function formColumns() {
